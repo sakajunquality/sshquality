48b4169c4893d61ee5013a2d1486dc5b229d5900
Jun SAKATA <jun.bj141400@gmail.com>
2017-02-13 17:21:16 +0900

    add init command. generate combined config.

--- resources/host.go
+++ resources/host.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"os/user"
 )
 
@@ -20,7 +21,22 @@ type HostConfig struct {
 	IdentityFile string
 }
 
-func WriteSshConfig(hosts []Host, config HostConfig, config_name string) {
+func InitSshQuality() {
+	usr, _ := user.Current()
+	ssh_dir := usr.HomeDir + "/.ssh"
+
+	config_dir := ssh_dir + "/conf.d"
+	key_dir := ssh_dir + "/keys"
+
+	// @todo create if not exist
+	os.MkdirAll(config_dir, 0755)
+	os.MkdirAll(key_dir, 0755)
+
+	// @todo add option for this
+	exec.Command("cp", "-a", ssh_dir+"/config", ssh_dir+"/config.save").Run()
+}
+
+func WriteEachConfig(hosts []Host, config HostConfig, config_name string) {
 	var config_string string
 
 	for _, host := range hosts {
@@ -37,8 +53,22 @@ func WriteSshConfig(hosts []Host, config HostConfig, config_name string) {
 	config_dir := usr.HomeDir + "/.ssh/conf.d"
 	file_name := config_dir + "/" + config_name + ".conf"
 
-	os.MkdirAll(config_dir, 0755)
 	ioutil.WriteFile(file_name, content, 0644)
 
-	fmt.Printf("created ssh config to %s\n", file_name)
+	fmt.Printf("Created ssh config to %s\n", file_name)
+}
+
+func WriteSshConfig() {
+	usr, _ := user.Current()
+	ssh_dir := usr.HomeDir + "/.ssh"
+
+	// @todo add option for this
+	exec.Command("cp", "-a", ssh_dir+"/config", ssh_dir+"/config.save").Run()
+
+	// @todo refactor
+	// exec.Command cannot use Redirect or pipeline
+	generate_cmd := "cat " + ssh_dir + "/conf.d/* > " + ssh_dir + "/config"
+	exec.Command("sh", "-c", generate_cmd).Run()
+
+	fmt.Printf("Overwrite ~/.ssh/config\n")
 }
